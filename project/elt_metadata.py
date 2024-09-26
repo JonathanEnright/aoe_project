2eb54d4337e3a818711fa088bfd7d6dadded826a
Jono <Jonathan.enright@live.com.au>
2024-09-26 23:40:29 +0100

    Refactor code to work better with json files and relic file upload

--- project/elt_metadata.py
+++ project/elt_metadata.py
@@ -1,4 +1,4 @@
-from utils import Config, timer, fetch_api_file
+from utils import Config, timer, fetch_api_json, create_s3_session
 from extract import validate_json_schema
 from extract import ApiSchema
 from load import load_json_data
@@ -16,7 +16,7 @@ def main():
     config = Config("config.yaml")
 
     # Setup:
-    s3 = None
+    s3 = create_s3_session()
     _base_url = config.stats_base_url
     _endpoint = config.metadata_endpoint
     _params = None
@@ -26,16 +26,16 @@ def main():
 
     # Extract phase
     logger.info("Starting data extraction.")
-    content = fetch_api_file(_base_url, _endpoint, _params)
+    json_data = fetch_api_json(_base_url, _endpoint, _params)
 
     # Validate phase
-    validated_data = validate_json_schema(content, _validation_schema)
+    validated_data = validate_json_schema(json_data, _validation_schema)
 
     # Load phase
     logger.info("Starting data loading.")
     s3 = load_json_data(validated_data, _output_prefix, _s3_bucket, s3)
+    logger.info("Script complete.")
 
 
 if __name__ == "__main__":
     main()
-    logger.info("Script complete.")
